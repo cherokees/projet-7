5bce2e2ee4a6ae63155f4840bcdb01a8dd5b7702
jordan <jvh78720@gmail.com>
2020-10-19 11:27:02 +0200

    mise en place du header (css pas terminé)

--- frontend/groupomania/src/App.js
+++ frontend/groupomania/src/App.js
@@ -6,7 +6,7 @@ import Header from './template/header';
 function App() {
   return (
     <div className="App">
-      <Header />
+      < Header />
     </div>
   );
 }
--- frontend/groupomania/src/template/header.js
+++ frontend/groupomania/src/template/header.js
@@ -1,11 +1,21 @@
 import styles from './header.css';
 import React from 'react';
+import logo from '../images/icon-left-font.png'
 
 class Header extends React.Component {
     render() {
         return (
             <div className='container_header'>
-                <div className='logo'></div>
+                <div className='logo' styles={{ backgroundImage: `url(${logo})` }}>
+                </div>
+                <div className='header_container_login'>
+                    <div className="login">
+                        <p>Login</p>
+                    </div>
+                    <div className="signup">
+                        <p>Signup</p>
+                    </div>
+                </div>
             </div>
         )
     }
