4e83fd2c5e7e930cf4120e189fd4619b6eb72c7f
jordan <jvh78720@gmail.com>
2020-10-19 10:54:30 +0200

    mis en en place de react dans la partie frontend

---
+++ backend/routes/usersRouter.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const { getAllUsers, addUser } = require('../control/users');
+const { validateFieldsPOST, VLD_NOT_EMPTY_STRING } = require('../utils/validator');
+const router = express.Router();
+
+router.get("/", async (req, res, next) => {
+    try {
+        const rows = await getAllUsers();
+        res.status(200).json({ data: rows });
+    } catch (err) {
+        console.error(err); // On affiche l'erreur côté serveur
+        res.status(500).json({ data: null, message: "Internal error" }); // L'erreur renvoyée est générique
+    }
+});
+
+router.post("/register",
+    validateFieldsPOST({
+        email: [VLD_NOT_EMPTY_STRING],
+        password: [VLD_NOT_EMPTY_STRING],
+        firstName: [VLD_NOT_EMPTY_STRING],
+        lastName: [VLD_NOT_EMPTY_STRING],
+    }),
+    async (req, res, next) => {
+        try {
+            const result = await addUser(
+                req.body.email,
+                req.body.password,
+                req.body.firstName,
+                req.body.lastName,
+            );
+            res.status(200).json({ data: { id: result }, message: "Utilisateur créé" });
+
+        } catch (err) {
+            console.error(err);
+            res.status(500).json({ data: null, message: "Erreur interne du serveur" });
+        }
+    }
+);
+
+export default router;
